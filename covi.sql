35369a5201721fd0690014b4cd2127b8df28891b
uditohri <vaishaliohri1@gmail.com>
2021-05-26 00:50:49 +0530

    Update covi.sql

--- covi.sql
+++ covi.sql
@@ -40,7 +40,7 @@ CREATE TABLE `bed` (
 
 LOCK TABLES `bed` WRITE;
 /*!40000 ALTER TABLE `bed` DISABLE KEYS */;
-INSERT INTO `bed` VALUES (1011,'Paras hospital','Gurugram','2506','icu',400,'9971547815');
+INSERT INTO `bed` VALUES (1001,'Jyoti Trust Hospital','Mumbai','Kondaji Chawl No 2, 3 9 400 012, VL Pednekar Marg, Bhoiwada, Parel-400014','Oxygen',40,'9967281605'),(1002,'Lok Nayak Hospital','New Delhi','metro Station Central, Jawaharlal Nehru Marg, near Delhi Gate-110002','Oxygen',38,'8046820570'),(1003,'Naidu Hospital','Pune','Behind Pune Main Railway station, off, Wellesley Rd-411001','Non-Oxygen',70,'9371333533'),(1004,'Jaipuria Hospita;','Jaipur','Near Railway Cross , Malviya Nagar , Jaipur-302017','Oxygen',45,'1412552034'),(1005,'Badar Hospital','Mumbai','339, Nawayat Mohalla,, New Eidgah Road, Nala Sopara, Maharashtra-401203','Oxygen',34,'9225807866'),(1006,'Om Hospital','Pune','Anjali Apartment, Vetalbaba Chowk, Senapati Bapat Rd, Shivajinagar-411016','Non-Oxygen',72,'9689931118'),(1007,'Mahatma Gandhi Hospital','Jaipur','RIICO Institutional Area, Tonk Rd, Sitapura, Jaipur, Rajasthan-302022','Non-Oxygen',80,'141 277 0798'),(1008,'SMS Hospital','Ahmedabad','Near Tapovan Circle, Visat - Gandhinagar Hwy, Chandkheda-382424','Oxygen',35,'75739 49408');
 /*!40000 ALTER TABLE `bed` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -68,6 +68,7 @@ CREATE TABLE `blood` (
 
 LOCK TABLES `blood` WRITE;
 /*!40000 ALTER TABLE `blood` DISABLE KEYS */;
+INSERT INTO `blood` VALUES (1009,'Manish Jain','New Delhi','O+','2021-01-15 00:00:00','9771853435'),(1010,'Khushi Jain','Mumbai','AB+','2020-11-17 00:00:00','9899744434'),(1011,'Rohan Sharma','Mumbai','O-','2021-03-04 00:00:00','9781260154'),(1012,'Yuvraj Bhatnagar','Jaipur','A+','2021-04-22 00:00:00','7439264142'),(1013,'Sneha Nagpal','New Delhi','AB-','2021-02-21 00:00:00','9958378666'),(1014,'Aadya Singhal','Ahmedabad','B+','2021-03-12 00:00:00','7995204721'),(1015,'Aashish Malhotra','Jaipur','O+','2021-04-09 00:00:00','8447700514'),(1016,'Mehak Saluja','Ahmedabad','O-','2021-05-02 00:00:00','9821275522');
 /*!40000 ALTER TABLE `blood` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,7 +83,6 @@ CREATE TABLE `equipment` (
   `equipment_id` int NOT NULL,
   `equipment_type` varchar(45) DEFAULT NULL,
   `city` varchar(45) DEFAULT NULL,
-  `address` varchar(100) DEFAULT NULL,
   `stock` int DEFAULT NULL,
   `price` double(8,2) DEFAULT NULL,
   `contact` varchar(15) DEFAULT NULL,
@@ -96,6 +96,7 @@ CREATE TABLE `equipment` (
 
 LOCK TABLES `equipment` WRITE;
 /*!40000 ALTER TABLE `equipment` DISABLE KEYS */;
+INSERT INTO `equipment` VALUES (1017,'Oxygen Cans','Chandigarh',70,2200.00,'9914401328'),(1018,'Oxygen Cans','New Delhi',90,3000.00,'8595629995'),(1019,'Oxygen Cans','Chandigarh',75,2500.00,'9915140300'),(1020,'Oxygen Concentrator','Pune',32,5000.00,'9175395460'),(1021,'Oxygen Concentrator','Mumbai',30,6000.00,'982786006'),(1022,'Oxygen Concentrator','New Delhi',25,4500.00,'8527710489'),(1023,'Oxygen Cylinder','New Delhi',12,3700.00,'8800886230'),(1024,'Oxygen Cylinder','Bhopal',15,3900.00,'9893590941');
 /*!40000 ALTER TABLE `equipment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -124,6 +125,7 @@ CREATE TABLE `medicine` (
 
 LOCK TABLES `medicine` WRITE;
 /*!40000 ALTER TABLE `medicine` DISABLE KEYS */;
+INSERT INTO `medicine` VALUES (1025,'Azithromycin','Gurugram','Koyal Vihar, Sector 52-122022',400,850.00,'9811455332'),(1026,'Azithromycin','Mumbai','Shripal Building Chhaya Society Siom Tromday Road Near SBI Bank Cha-400071',350,950.00,'8048861728'),(1027,'Azithromycin','New Delhi','Shri Aurobindo Marg, Safdarjung Campus, Ansari Nagar West-110029',200,900.00,'9910160011'),(1028,'Bevacizumab','Mumbai','98/779,rashmi mansion,motilal nagar no1,goregaon west-400104',250,11000.00,'8048013167'),(1029,'Bevacizumab','Jaipur','Shop No. 378, Near Dalda Factory, Durgapura, Jaipur-302018',200,10000.00,'7457182862'),(1030,'Fabiflu','New Delhi','N-9, N Block, Rampuri, Kalkaji-110019',375,1300.00,'8929871978'),(1031,'Ivermectin','Jaipur','A-212, 80 Feet Rd, Mahesh Nagar, Gopal Pura Mode-302015',250,200.00,'9829610673'),(1032,'Remdesivir','Mumbai','98/779,rashmi mansion,motilal nagar no1,goregaon west-400104',300,900.00,'8048013167');
 /*!40000 ALTER TABLE `medicine` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -136,4 +138,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-25  2:03:42
+-- Dump completed on 2021-05-26  0:49:39
