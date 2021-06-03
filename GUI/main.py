80aec75e260de388fb9e526bb03480d17effde79
Paarth Arya <63608051+PaarthArya@users.noreply.github.com>
2021-06-03 15:33:03 +0530

    Window Manager
    Added donor menu screen

--- GUI/main.py
+++ GUI/main.py
@@ -1,19 +1,27 @@
 import kivy
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-# creating the window elements for the application
-# main grid comprises the subgrid for buttons
-class MyGrid(Widget):
+# setting the screens
+class MainWindow(Screen):
+    pass
+
+class DonorMenu(Screen):
     pass
 
 class CoviCare(App):
     def build(self):
-        return MyGrid()
+        # setting the screen manager 
+        sm = ScreenManager()
+        # adding screens through widgets
+        sm.add_widget(MainWindow(name = "main"))
+        sm.add_widget(DonorMenu(name = "donormenu"))
+        return sm
 
 if __name__ == "__main__":
     CoviCare().run()
