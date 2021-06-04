b43cdd7ed10652c0b844841b3b0fc5f69f515736
Paarth Arya <63608051+PaarthArya@users.noreply.github.com>
2021-06-05 01:04:23 +0530

    Medicine Database Done

--- GUI/main.py
+++ GUI/main.py
@@ -2,24 +2,76 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
+from kivy.properties import ObjectProperty
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
+import mysql.connector
+
+
+# open database connection
+conn = mysql.connector.connect(host="localhost", user="root", password="Covicare123", database='covicare')
+
+# prepare a cursor object using cursor() method
+mycur = conn.cursor()
+
+
+# global variables
+city = ""
 
 
 # setting the screens
+class SupplyMenu(Screen):
+    supplycity = ObjectProperty(None)
+
+    def assigncity(self):
+        global city
+        city = self.city.text
+
+    def resetcity(self):
+        self.city.text = ""
+
+
+class SupplyMedicine(Screen):
+    medid = ObjectProperty(None)
+    medname = ObjectProperty(None)
+    medaddress = ObjectProperty(None)
+    medstock = ObjectProperty(None)
+    medprice = ObjectProperty(None)
+    medcontact = ObjectProperty(None)
+
+    def assignmedicine(self):
+        id = self.medid.text
+        name = self.medname.text
+        address = self.medaddress.text
+        stock = self.medstock.text
+        price = self.medprice.text
+        contact = self.medcontact.text
+
+        sql = "INSERT INTO MEDICINE VALUES(%s, %s, %s, %s, %s, %s, %s);"
+        val = (id, name, city, address, stock, price, contact)
+        mycur.execute(sql, val)
+        conn.commit()
+     
+    def resetmedicine(self):
+        self.medid.text = ""
+        self.medname.text = ""
+        self.medaddress.text = ""
+        self.medstock.text = ""
+        self.medprice.text = ""
+        self.medcontact.text = ""
+
+
 class MainWindow(Screen):
     pass
 
 
-class SupplyMenu(Screen):
-    pass
+# DEFINE ALL GLOBAL FUNCTIONS
 
 
-class SupplyMedicine(Screen):
-    pass
 
+# ---------------------------
 
 class CoviCare(App):
     def build(self):
@@ -31,9 +83,11 @@ class CoviCare(App):
         sm.add_widget(SupplyMedicine(name="supplymedicine"))
         return sm
 
-#    def save_supplycity(self): 
-#        supplycity = self.root.ids.supplycity.text
-#        print(supplycity)
+    def supplyCity(self): 
+        x = self.root.ids.supplycity.text
+        print(x)
+
 
 if __name__ == "__main__":
     CoviCare().run()
+    conn.close
