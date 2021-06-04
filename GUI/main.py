1314cdb68ba0509bf173ac0f3f3012e1c72531cb
Mohak634 <68562942+Mohak634@users.noreply.github.com>
2021-06-04 23:39:30 +0530

    added medicine menu

--- GUI/main.py
+++ GUI/main.py
@@ -7,21 +7,33 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+
 # setting the screens
 class MainWindow(Screen):
     pass
 
-class DonorMenu(Screen):
+
+class SupplyMenu(Screen):
     pass
+
+
+class SupplyMedicine(Screen):
+    pass
+
 
 class CoviCare(App):
     def build(self):
         # setting the screen manager 
         sm = ScreenManager()
         # adding screens through widgets
-        sm.add_widget(MainWindow(name = "main"))
-        sm.add_widget(DonorMenu(name = "donormenu"))
+        sm.add_widget(MainWindow(name="main"))
+        sm.add_widget(SupplyMenu(name="supplymenu"))
+        sm.add_widget(SupplyMedicine(name="supplymedicine"))
         return sm
+
+#    def save_supplycity(self): 
+#        supplycity = self.root.ids.supplycity.text
+#        print(supplycity)
 
 if __name__ == "__main__":
     CoviCare().run()
